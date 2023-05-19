b6b285c0b9792b72f0b4dea6a67f54f6cc07f0fb
Ed Plitt <ecplitt@optonline.net>
2023-05-19 08:00:37 -0400

    Second Commit

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, jsonify
+import os
+
+app = Flask(__name__)
+
+img = os.path.join('static', 'Image')
+
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data-Analyst',
+    'location': 'Bengaluru, India',
+    'salary': 'Rs. 10,00,000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Delhi, India',
+    'salary': 'Rs. 15,00,000'
+  },
+  {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Remote'
+  },
+  {
+    'id': 4,
+    'title': 'Backend Engineer',
+    'location': 'San Francisco, USA',
+    'salary': '$150,000'
+  }
+]
+
+@app.route('/')
+# def home():
+#     file = os.path.join(img, 'mtn2.jpeg')
+#     return render_template('image_render.html', image=file)
+
+def hello_jovian():
+    return render_template('home.html',
+                           jobs=JOBS,
+                           company_name='Jovian')
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',debug=True, port=9000)
+
